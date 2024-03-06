ff7ce37011a93b5a9faef4a7435cb8313c52f9fd
clouwud <dakshgajbhiye902@gmail.com>
2024-03-06 18:46:30 +0530

    init

--- String4.java
+++ String4.java
@@ -1,6 +1,7 @@
 public class String4 {
     public static void main(String[] args) {
-        String s1-"my name is daksh. my name is bob. my name is sono.";
-        String replaString=s1.replace(s1, s1)
+        String s1 = "my name is daksh. my name is bob. my name is sono.";
+        String replaceString = s1.replaceAll("\\s", "");
+        System.out.println(replaceString);
     }
 }
