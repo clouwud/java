cafbc6ea5204fd281b90f4f677da54f106a72709
clouwud <dakshgajbhiye902@gmail.com>
2024-03-06 22:18:00 +0530

    init

---
+++ String5.java
@@ -0,0 +1,7 @@
+public class String5 {
+    public static void main(String[] args) {
+        String s1 = "HELLO EVERYONE this is a STRING";
+        String s1lower = s1.toLowerCase();
+        System.out.println(s1lower);
+    }
+}
