34b5d77bc9738e389bfa1ea2ee413ffd0ac6255f
clouwud <dakshgajbhiye902@gmail.com>
2024-03-06 22:27:50 +0530

    init

---
+++ String7.java
@@ -0,0 +1,8 @@
+public class String7 {
+    public static void main(String args[]) {
+        String s1 = "java String";
+        String s2 = "India";
+        s1.concat(s2);
+        System.out.println(s1);
+    }
+}
---
+++ String8.java
@@ -0,0 +1,7 @@
+public class String8 {
+    public static void main(String[] args) {
+        String name = "java";
+        char ch = name.charAt(3);
+        System.out.println(ch);
+    }
+}
